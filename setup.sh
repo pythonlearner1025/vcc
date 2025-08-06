bee3e83ec86ba01c4fa727b98a107e845d25ba4a
pythonlearner1025 <mjsong2021@gmail.com>
2025-08-06 11:19:01 -0700

    install muon

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 pip install numpy pandas scanpy[skmisc] gcsfs pyarrow h5py tqdm torch matplotlib seaborn wandb nvitop cell-eval adam-atan2
+pip install git+https://github.com/KellerJordan/Muon
 
 cp data/competition_support/competition_train.h5 /
 cp esm_all.pt /
